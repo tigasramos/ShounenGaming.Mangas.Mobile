266241316b2cfc9f54a594756c7a96fc980f6cb7
Tiago Ramos <tiago_bras_ramos@hotmail.com>
2023-10-26 15:49:35 +0100

    Playstore Release

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,6 @@ import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shounengaming_mangas_mobile/src/features/app/sg_mangas_app.dart';
 
-/*
-https://pub.dev/packages/flutter_autoupdate
- Current Version: package_info_plus
- Releases Website: https://api.github.com/repos/tigasramos/ShounenGaming.Mangas.Mobile/releases
-*/
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -50,7 +44,7 @@ Future<void> main() async {
           'https://415f2b7ffe5d463f99aba8029ff53b1a@glitch.shounengaming.xyz/1';
       options.tracesSampleRate = 1.0;
       options.attachScreenshot = true;
-      options.environment = "local";
+      options.environment = "prod";
 
       options.release = "${packageInfo.version}.${packageInfo.buildNumber}";
     },
@@ -68,9 +62,9 @@ final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
 });
 
 final serverUrlProvider = StateProvider((ref) {
-  return 'https://localhost:7252/';
+  //return 'https://localhost:7252/';
   //return 'https://server-dev.shounengaming.xyz/';
-  //return 'https://server.shounengaming.xyz/';
+  return 'https://server.shounengaming.xyz/';
 });
 
 final dioProvider = Provider<Dio>((ref) {
